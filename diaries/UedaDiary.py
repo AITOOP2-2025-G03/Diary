ba47c1f14468ee473e4d48bdeadc882b6cb65a37
yusei <youshengjiubaotian668@gmail.com>
2025-10-22 11:56:31 +0900

    修正

--- diaries/UedaDiary.py
+++
@@ -1,9 +0,0 @@
-from diaries.AbstractDiary import AbstractDiary
-
-class UedaDiary(AbstractDiary):
-    def get_date(self):
-        return "2025-10-16"
-    def get_summary(self):
-        return "よくわかりません"
-    def get_author(self):
-        return "Ueda"
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from diaries.DiarySample import DiarySample
-from diaries.KubotaDiary import KubotaDiary
 from diaries.HaruDiary import HaruDiary
-from diaries.TakumiDiary import TakumiDiary
+
 # ↓のリストには、メンバーの各日記が格納されます。
 
 diaries = [
     DiarySample(), 
-    KubotaDiary(), 
     HaruDiary(),
-    TakumiDiary(), 
-    UedaDiary(), 
     ]
 
 for d in diaries:
