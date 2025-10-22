0940542cefde4acc2b6f585f9f95a69448e805af
yusei <youshengjiubaotian668@gmail.com>
2025-10-22 11:44:44 +0900

    間違いの修正

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from diaries.DiarySample import DiarySample
 from diaries.HaruDiary import HaruDiary
-from diaries.TakumiDiary import TakumiDiary
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(), 
     HaruDiary(),
-    TakumiDiary(), 
     ]
 
 for d in diaries:
